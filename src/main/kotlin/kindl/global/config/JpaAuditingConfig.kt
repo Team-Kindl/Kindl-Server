09129f4e6c5b3e08f7c09de663e3820153bee299
kyoooooong <haileyre000b@naver.com>
2026-06-23 19:10:49 +0900

    feat: #1 JPA Auditing, AuditorAware 설정 추가

---
+++ src/main/kotlin/kindl/global/config/JpaAuditingConfig.kt
@@ -0,0 +1,14 @@
+package kindl.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+class JpaAuditingConfig {
+
+    @Bean
+    fun auditorAware(): AuditorAware<String> = AuditorAwareImpl()
+}
