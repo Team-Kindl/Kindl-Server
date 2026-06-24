3db0770dd57961d7b0ca915db1496bac93d8afb4
kyoooooong <haileyre000b@naver.com>
2026-06-25 02:40:40 +0900

    feat: #3 토큰 관련 에러코드 추가

--- src/main/kotlin/kindl/global/exception/ErrorCode.kt
+++ src/main/kotlin/kindl/global/exception/ErrorCode.kt
@@ -7,6 +7,7 @@ enum class ErrorCode(
     val code: String,
     val message: String,
 ) {
+    /* ========== 공통 ========== */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "ISE", "서버 내부 오류입니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "FBD", "권한이 없습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNA", "인증되지 않았습니다."),
@@ -14,4 +15,9 @@ enum class ErrorCode(
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "IVP", "요청 파라미터 타입/형식이 올바르지 않습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "NF", "존재하지 않는 리소스입니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "BR", "잘못된 요청입니다."),
+
+    /* ========== 인증/토큰 ========== */
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "ETK", "만료된 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "ITK", "유효하지 않은 토큰입니다."),
+    INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "ITT", "토큰 타입이 올바르지 않습니다."),
 }
